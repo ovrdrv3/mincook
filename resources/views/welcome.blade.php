e180840d9c770282671b618ded6979c30ad43686
Donaven Snowden <donaven.snowden@gmail.com>
2018-07-30 21:01:13 -0700

    bring in home page from concept site, adjust CSS for create form 👌

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,112 +1,105 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts.app')
 
-        <title>Create a Recipe</title>
-
-        <!-- CSS and Fonts -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
-
-    </head>
-    <body>
-        <div id="app" class="container my-5">
-          <H1>Add a new recipe</H1>
-                <div class="form-group">
-                    <input class="form-control" placeholder="Name" v-model="recipe.name">
-                </div>
-                <div class="form-group">
-                    <textarea class="form-control" placeholder="Description" v-model="recipe.description"></textarea>
-                </div>
-
-                <h2>Ingredients</h2>
-
-                <div class="form-group">
-                    <button class="form-control" @click="addIngredient(false)">Add Ingredient</button>
-                </div>
-
-                <div class="form-row"
-                v-for="(ingredient, index) in recipe.ingredients">
-
-                  <div class="col-md-2 mb-3">
-                    <label for="amount" v-show="index == 0">Amount</label>
-                    <input type="text"
-                    :ref="'amount'"
-                    @keydown.enter="$refs.ingredient[index].focus"
-                    @keyup.delete="removeStepWithBackspace('ingredients', index)"
-                    class="form-control" v-model="ingredient.amount" :placeholder="'# ' + (index + 1) + ' Amount'">
-                  </div>
-
-                  <div class="col-md-10 mb-3">
-                    <label for="food" v-show="index == 0">Food</label>
-                    <div class="input-group">
-                      <input
-                      @keydown.enter="addIngredient(true)"
-                      @keyup.delete="removeStepWithBackspace('ingredients', index)"
-                      :ref="'ingredient'"
-                      v-model="ingredient.food"
-                      type="text" class="form-control" :placeholder="'# ' + (index + 1) + ' Food'" required>
-                    </div>
-                  </div>
-                </div>
-                <hr>
-
-                <h2>Instructions</h2>
-
-                <div class="form-group">
-                    <button class="form-control" @click="addInstruction(false)">Add Step</button>
-                </div>
-
-                <div class="form-row"
-                v-for="(instruction, index) in recipe.instructions">
-                  <div class="col-md-1 mb-3">
-                    <label for="sort" v-show="index == 0">Order</label>
-
-                    <input
-                    :value="index + 1"
-                    type="text" class="form-control" tabindex="-1" style="background-color:transparent; border: 0; font-size: 1em;" readonly>
-
-                  </div>
-                  <div class="col-md-10 mb-3">
-                    <label for="do" v-show="index == 0">Step</label>
-                    <div class="input-group">
-                      <textarea
-                      :ref="'step'"
-                      @keydown.enter="addInstruction(true)"
-                      @keyup.delete="removeStepWithBackspace('instructions', index)"
-                      v-model="instruction.do" class="form-control"
-                      rows="3" required></textarea>
-                    </div>
-                  </div>
-                  <div class="col-md-1 mb-3 center-text">
-                    <label for="buttons" v-show="index == 0">Change</label>
-                    <br v-if="index == 0">
-
-                    <font-awesome-icon icon="arrow-up" size="lg"
-                    v-if="index != 0"
-                    @click="shiftStepUp('instructions', index)"></font-awesome-icon>
-
-                    <font-awesome-icon icon="arrow-down" size="lg"
-                    v-if="index != recipe.instructions.length - 1"
-                    @click="shiftStepDown('instructions', index)"></font-awesome-icon>
-
-                    <font-awesome-icon icon="trash" size="lg"
-                    v-if="index != 0"
-                    @click="removeStep('instructions', index)"></font-awesome-icon>
-
-                  </div>
-                </div>
-
-
-                <div class="form-group">
-                    <button class="form-control" @click.prevent="addRecipe">Submit</button>
-                </div>
+@section('content')
+<h1 id="top-header" class="primary-font section">
+    minimal
+    <br>
+    <span id="changing-text" class="slide-in-animation">cook</span>
+  </h1>
+    <div class="row">
+      <div class="section col-md-5">
+        <H2 class="primary-font">Simple Cooking.</H2>
+        <p>
+          Hmm, a home-cooked meal would be nice tonight. You find a recipie that seems simple, but scroll down to find a huge list of ingredients, and a complex set of instructions. Please spend some time here, and I am sure you will agree that cooking can be simple.
+        </p>
+        <p><a class="" href="/recipies">browse recipies now</a></p>
+      </div>
+      <div class="section col-md-5 offset-md-2">
+        <H2 class="primary-font">Solutions.</H2>
+          <br>
+          <p>
+            Easy simple cooking, with a minimal amount of ingredients, cooking with one pan, bulk ingredients that won't go bad in your pantry. Does this sound good to you? This type of cooking is faster yet still tasty.
+        </p>
+      </div>
+      <div class="section col-md-5 offset-md-2">
+          <H2 class="primary-font">Problems.</H2>
+          <br>
+          <p>
+            Maybe you never learned how to cook, spend too much money eating out, or have minimal access to a kitchen. Give your stove top (or hot plate) some life and see how quickly these problems are eased.
+          </p>
+      </div>
+      <div class="section col-md-5 offset-md-2">
+          <H2 class="primary-font">Technique.</H2>
+          <br>
+          <p>
+            Want to cut your food prep time? Want your greens to look fancy instead of crumbled up? Improve on a few of your techniques and you'll have good looking plates in no time.
+          </p>
+      </div>
+      </div>
+  <footer class="pt-4 my-md-5 pt-md-5 border-top light-purple">
+    <div class="container">
+          <div class="row">
+            <div class="col-12 col-md">
+        <h2 class="primary-font">minimal <br> cook </h2>
+            </div>
+            <div class="col-6 col-md">
+              <h5>Features</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="" href="#">Cool stuff</a></li>
+                <li><a class="" href="#">Random feature</a></li>
+                <li><a class="" href="#">Team feature</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5>Resources</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="" href="#">Resource</a></li>
+                <li><a class="" href="#">Resource name</a></li>
+                <li><a class="" href="#">Another resource</a></li>
+                <li><a class="" href="#">Final resource</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5>About</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="" href="#">Team</a></li>
+                <li><a class="" href="#">Locations</a></li>
+                <li><a class="" href="#">Privacy</a></li>
+                <li><a class="" href="#">Terms</a></li>
+              </ul>
+            </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md">
+          <small class="d-block mb-3 ">Developed by Donaven Snowden © 2018</small>
         </div>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    </body>
+      </div>
+      </div>
+    </footer>
 
-</html>
+
+
+
+
+
+
+<script>
+
+
+let textArray = [ "ingredients", "space", "time", "money", "mess."];
+
+var textToChange = document.getElementById("changing-text");
+
+textToChange.addEventListener("animationiteration", setTextContent, false);
+var currentIndex = 0;
+
+function setTextContent(){
+  textToChange.textContent = textArray[currentIndex++];
+  if(currentIndex == textArray.length){currentIndex = 0}
+}
+
+
+
+</script>
+
+@endsection
